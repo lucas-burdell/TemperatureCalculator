814978d39ab0723f1a6fd56480d5c9dafd479f4e
Lucas Burdell <lucas.burdell@gmail.com>
2017-05-23 20:43:59 -0500

    add all temps

--- assets/js/tempCalculator.js
+++ assets/js/tempCalculator.js
@@ -12,16 +12,79 @@ function formatTemp(num, decimals) {
 }
 
 function toCelsius(num, type) {
-    if (type == "fahrenheit") {
-        
+    if (type == "celsius") {
+        return num;
+    } else if (type == "fahrenheit") {
+        return (num - 32) * (5./9.);
+    } else if (type == "kelvin") {
+        return (num - 273.15);
+    } else if (type == "rankine") {
+        return (num - 491.67) * (5./9.);
+    } else if (type == "delisle") {
+        return 100 - (num * 2. / 3.);
+    } else if (type == "newton") {
+        return num * 100./33.
+    } else if (type == "reaumur") {
+        return (num * 5./4.);
+    } else {
+        return (num - 7.5) * (40./21.);
     }
 }
 
+function toFahrenheit(num) {
+    return (num * 9./5.) + 32;
+}
+
+function toKelvin(num) {
+    return num + 273.15;
+}
+
+function toRankine(num) {
+    return (num + 273.15) * (9./5.);
+}
+
+function toDelisle(num) {
+    return (100 - num) * (3./2.);
+}
+
+function toNewton(num) {
+    return (num * 33./100.);
+}
+
+function toReaumur(num) {
+    return (num * 4. / 5.);
+}
+
+function toRomer(num) {
+    return num * 21./40. + 7.5;
+}
 
 function convertTemp() {
     var temperatureToConvert = parseFloat($("#inputTemp").val());
+    var tempType = $("#tempSelect").val();
+    var celsius = toCelsius(temperatureToConvert, tempType);
+    var fahrenheit = formatTemp(toFahrenheit(celsius), 6);
+    var kelvin = formatTemp(toKelvin(celsius), 6);
+    var rankine = formatTemp(toRankine(celsius), 6);
+    var delisle = formatTemp(toDelisle(celsius), 6);
+    var newton = formatTemp(toNewton(celsius), 6);
+    var reaumur = formatTemp(toReaumur(celsius), 6);
+    var romer = formatTemp(toRomer(celsius), 6);
+    celsius = formatTemp(celsius, 6);
     
-    // test code
+    $("#fahrenheit").text(fahrenheit);
+    $("#celsius").text(celsius);
+    $("#kelvin").text(kelvin);
+    $("#rankine").text(rankine);
+    $("#delisle").text(delisle);
+    $("#newton").text(newton);
+    $("#reaumur").text(reaumur);
+    $("#romer").text(romer);
+    $("#tempData").text("Your input: " + (formatTemp(temperatureToConvert, 6)) + $("#tempSelect").find(':selected').text());
+    $("#inputTemp").val('');
+    $("#tempModal").modal('show');
+    /*
+     *
     var celsius = (temperatureToConvert - 32) * (5.0/9.0);
     // var ptext = temperatureToConvert.toFixed(3).concat(" in Fahrenheit is ").concat(celsius.toFixed(3)).concat(" in celsius!");
     // var ptext = sprintf("%.6f in Fahrenheit is %.6f in celsius!", temperatureToConvert, celsius);
@@ -32,6 +95,5 @@ function convertTemp() {
     console.log(celsius);
     $("#tempModal").modal('show');
     $("#inputTemp").val('');
+    */
 }
-
-//$("#convertTempButton").click(function(){ convertTemp(); });
